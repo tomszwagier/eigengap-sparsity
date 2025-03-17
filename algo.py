9c5d4c37d96856daa315786365a305c3ccaefd08
Tom-Szwagier <tom.szwagier1@gmail.com>
2025-03-17 14:51:10 +0100

    add psa and sample cov in the experiments

--- algo.py
+++ algo.py
@@ -1,7 +1,13 @@
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 import autograd.numpy as np  # Thinly-wrapped numpy
 from autograd import grad    # The only autograd function you may ever need
 from scipy.optimize import isotonic_regression
 from scipy.stats import ortho_group
+from time import time
+
+from psa import candidate_models, model_selection
 
 
 def eigengap_cst(p):
@@ -21,14 +27,14 @@ def project_orthogonal(V):
 
 
 def project_cone(lbda):
-    res = isotonic_regression(lbda, weights=None, increasing=False)
-    return res.x  # N.B: if at some point I need the flag type, it can be obtained by `np.diff(res.blocks)`.
+    res = isotonic_regression(lbda, weights=None, increasing=False)  # N.B: if at some point I need the flag type, it can be obtained by `np.diff(res.blocks)`.
+    return np.clip(res.x, 1e-10, 1e10)
 
 
 def pcd(loss_func, X, lr=0.01, max_iter=1000, tol=1e-6):
     steps = []
     count = 0
-    V, lbda = ortho_group.rvs(dim=X.shape[0]), np.random.normal(loc=1, scale=1.0, size=X.shape[0])
+    V, lbda = ortho_group.rvs(dim=X.shape[0]), np.random.normal(loc=1, scale=1.0, size=X.shape[0])  # TODO: CAUTION init?
     prev_loss = np.inf
     while count < max_iter:
         V = V - lr * grad(lambda V_: loss_func(lbda, V_, X))(V)
@@ -36,25 +42,47 @@ def pcd(loss_func, X, lr=0.01, max_iter=1000, tol=1e-6):
         lbda = lbda - lr * grad(lambda lbda_: loss_func(lbda_, V, X))(lbda)
         lbda = project_cone(lbda)
         new_loss = loss_func(lbda, V, X)
-        print(new_loss)
-        if (count > 0) and (abs(prev_loss - new_loss / prev_loss) < tol):  # replace with criterion on the gradient?
+        steps.append(new_loss)
+        if (count > 0) and (abs((prev_loss - new_loss) / prev_loss) < tol):  # replace with criterion on the gradient?
             break
         count += 1
         prev_loss = new_loss
-    return V, lbda
+    return V, lbda, steps
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # TODO: should not we remove sample mean even if we assume it is 0?
     np.random.seed(42)
 
-    n, p = 20, 10
-    X = np.random.multivariate_normal(mean=np.zeros(p,), cov=np.eye(p), size=n).T  # CAUTION X must be in p \times n shape
+    n, p = 40, 20  # 20, 10 / 40, 20 / 200, 100 / ...
+    X = np.random.multivariate_normal(mean=np.zeros(p,), cov=np.eye(p), size=n).T
 
+    # Sample covariance matrix
+    start = time()
     S = (1 / n) * X @ X.T
     eigval, eigvec = np.linalg.eigh(S)
-    print(eigval, eigvec)
+    eigval, eigvec = eigval[::-1], eigvec[::-1]
+    print("Time sample cov. =", time() - start)
+    print("Pen. lik. sample cov. =", penalized_normal_loglikelihood(eigval, eigvec, X))
+    print("Eigenvalues sample cov.", eigval)
 
-    V, lbda = pcd(penalized_normal_loglikelihood, X, lr=0.01, max_iter=1000, tol=1e-6)
-    print(V, lbda)
+    # Principal subspace analysis
+    start = time()
+    print("begin")
+    models = candidate_models(p)
+    print("done")
+    model_best, eigval_psa, eigvec_psa = model_selection(X, models)
+    print("Time PSA =", time() - start)
+    print("Pen. lik. PSA =", penalized_normal_loglikelihood(eigval_psa, eigvec_psa, X))
+    print("Eigenvalues PSA", eigval_psa)
+
+    # Eigengap sparsity
+    start = time()
+    V, lbda, steps = pcd(penalized_normal_loglikelihood, X, lr=0.01, max_iter=1000, tol=1e-6)
+    print("Time ESCP =", time() - start)
+    print("Pen. lik. ESCP =", penalized_normal_loglikelihood(lbda, V, X))
+    print("Eigenvalues ESCP", lbda)
+    plt.figure()
+    plt.plot(steps)
+    plt.show(block=True)
 
 
---
+++ psa.py
@@ -0,0 +1,65 @@
+import itertools
+import numpy as np
+
+
+def candidate_models(p):
+    """ Generate the whole family of Principal Subspace Analysis (PSA) models.
+    """
+    models = []
+    for seps in list(itertools.product(*((False, True),)*(p-1))):
+        model = [1]
+        for sep in seps:
+                if sep:
+                    model.append(1)
+                else:
+                    model[-1] += 1
+        models.append(model)
+    return models
+
+
+def kappa(model):
+    """ Compute the number of free parameters of a PSA model.
+    """
+    p = np.sum(model)
+    kappa_mu = p
+    kappa_eigvals = len(model)
+    kappa_eigenspaces = int(p * (p - 1) / 2 - np.sum(np.array(model) * (np.array(model) - 1) / 2))
+    return kappa_mu + kappa_eigvals + kappa_eigenspaces
+
+
+def ll(model, eigval, n):
+    """ Compute the maximum log-likelihood of a PSA model from the sample eigenvalues.
+    eigval must be sorted in decreasing order.
+    """
+    p = np.sum(model)
+    q_list = (0,) + tuple(np.cumsum(model))
+    eigval_mle = np.concatenate([[np.mean(eigval[qk:qk_])] * gamma_k for (qk, qk_, gamma_k) in zip(q_list[:-1], q_list[1:], model)])
+    return - (n / 2) * (p * np.log(2 * np.pi) + np.sum(np.log(eigval_mle)) + p), eigval_mle
+
+
+def bic(model, eigval, n):
+    """ Compute the Bayesian Information Criterion (BIC) of a PSA model from the sample eigenvalues.
+    eigval must be sorted in decreasing order.
+    """
+    ll_, eigval_mle = ll(model, eigval, n)
+    return kappa(model) * np.log(n) - 2 * ll_, eigval_mle
+
+
+def model_selection(X, models):
+    """ Perform model selection by minimizing the BIC among a family of candidate models.
+    """
+    p, n = X.shape
+    S = (1 / n) * X @ X.T
+    eigval, eigvec = np.linalg.eigh(S)
+    eigval, eigvec = eigval[::-1], eigvec[::-1]
+    model_best, eigval_best, crit_best = None, None, np.inf
+    for model in models:
+        crit_model, eigval_mle = bic(model, eigval, n)
+        if crit_model < crit_best:
+            model_best, eigval_best, crit_best = model, eigval_mle, crit_model
+    return model_best, eigval_best, eigvec
+
+
+if __name__ == "__main__":
+    print(candidate_models(5))
+    print([np.sum(model) for model in candidate_models(5)])
